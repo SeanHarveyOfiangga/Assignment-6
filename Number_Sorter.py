b2d2e7fbf474ef838a65f2efcb1fb5e48fb5da59
SeanHarveyOfiangga <seanharveyofiangga@gmail.com>
2021-11-28 17:59:32 +0800

    Done if the highest number is the first and the second number is the second

--- Number_Sorter.py
+++ Number_Sorter.py
@@ -2,7 +2,7 @@
 #Print the 4 numbers from highest to lowest using only if-else statement.
 
 def intro():
-    print ("\nWelcome to number sorter app!")
+    print ("\nWelcome to Number Sorter app!")
 
 intro()
 
@@ -14,7 +14,24 @@ Fourth = int(input("Please enter your Fourth number: "))
 def Numbers():
     if First >= Second and First >= Third and First >= Fourth:
         One = First
-        print (One)
+        if Second >= Third and Second >= Fourth:
+            Two = Second
+            if Third >= Fourth:
+                Three = Third
+                Four = Fourth
+                print (f"The highest number is '{One}', then '{Two}', then '{Three}', and the lowest number is '{Four}.'")
+                print ("Thank you for using Number Sorter App!")
+                exit()
+            if Fourth >= Third:
+                Three = Fourth
+                Four = Three
+                print (f"The highest number is '{One}', then '{Two}', then '{Three}', and the lowest number is '{Four}.'")
+                print ("Thank you for using Number Sorter App!")
+                exit()
+        if Third >= Second and Third >= Fourth:
+            Two = Third
+        if Fourth >= Second and Fourth >= Third:
+            Two = Fourth
     elif Second >= First and Second >= Third and Second >= Fourth:
         One = Second
         print (One)
