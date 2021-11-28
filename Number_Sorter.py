ef2bb06ed2e64da804148a004132731fb927c0bb
SeanHarveyOfiangga <seanharveyofiangga@gmail.com>
2021-11-28 17:42:04 +0800

    redesigned the program

--- Number_Sorter.py
+++ Number_Sorter.py
@@ -6,17 +6,22 @@ def intro():
 
 intro()
 
-def numbers():
-    First = int(input("\nPlease enter your first number: "))
-    Second = int(input("Please enter your Second number: "))
-    Third = int(input("Please enter your Third number: "))
-    Fourth = int(input("Please enter your Fourth number: "))
-    One = First
-    if One < Second:
+First = int(input("\nPlease enter your first number: "))
+Second = int(input("Please enter your Second number: "))
+Third = int(input("Please enter your Third number: "))
+Fourth = int(input("Please enter your Fourth number: "))
+
+def Numbers():
+    if First >= Second and First >= Third and First >= Fourth:
+        One = First
+        print (One)
+    elif Second >= First and Second >= Third and Second >= Fourth:
         One = Second
-        if One < Third:
-            One = Third
-        if One < Fourth:
-            One = Fourth
-    print (One)
-numbers()
+        print (One)
+    elif First >= First and First >= Second and First >= Fourth:
+        One = Third
+        print (One)
+    elif Fourth >= First and Fourth >= Third and Fourth >= Second:
+        One = Fourth
+        print (One)
+Numbers()
